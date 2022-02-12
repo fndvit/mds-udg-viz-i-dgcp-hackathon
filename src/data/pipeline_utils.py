10089c8614ece9aa5dd5272709a1040fd6881429
Victor Micó <vicmibio@gmail.com>
2022-02-12 19:00:23 +0100

    Adding feature temperature to anomalies from pipeline extractor

--- src/data/pipeline_utils.py
+++ src/data/pipeline_utils.py
@@ -3,6 +3,19 @@ import pandas as pd
 from raster2xyz.raster2xyz import Raster2xyz
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
+import os
+
+cwd = os.getcwd()
+repo_path = os.path.abspath(os.path.join(cwd, os.pardir, os.pardir))
+data_path = os.path.join(repo_path, 'data')
+data_processed_path = os.path.join(repo_path, 'data', 'processed')
+
+avg_file_path = os.path.join(data_processed_path, 'avg-2012-2021.npy')
+sd_file_path = os.path.join(data_processed_path, 'sd-2012-2021.npy')
+
+avg = np.load(avg_file_path, allow_pickle=True)
+sd = np.load(sd_file_path, allow_pickle=True)
+
 
 def tiff_to_csv(tiff_path, csv_path):
     # Transform TIFF file from tiff_path into pandas DataFrame
@@ -20,11 +33,17 @@ def tiff_to_csv(tiff_path, csv_path):
     pd.DataFrame(temp).to_csv('temp.csv')
 
 
+def temperature_to_anomalies(input_csv_path, output_csb_path, month):
+    x = np.array(pd.read_csv(input_csv_path, header=None).replace(99999.0, 0))
+    # Subtract average
+    x -= avg[month]
+    # Divide sd and return 0 if sd is 0 for that point
+    x = np.divide(x, sd[month], out=np.zeros_like(x), where=sd[month] != 0)
 
 
 def csv_to_png(csv_path, png_path):
-    cmap=LinearSegmentedColormap.from_list('rg', ["blue", "w", "r"], N=256)
+    cmap = LinearSegmentedColormap.from_list('rg', ["blue", "w", "r"], N=256)
     t = np.array(pd.read_csv(csv_path, header=None).replace(99999.0, 0))
-    plt.figure(figsize=(36,18))
+    plt.figure(figsize=(36, 18))
     plt.imshow(t, cmap=cmap, interpolation='nearest')
     plt.savefig(png_path)
